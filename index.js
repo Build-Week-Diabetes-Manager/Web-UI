9786ad876fe150d1741157a186eeb7d73316c874
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-08-28 15:16:05 -0500

    Added mobile version of fade-in effect on index.html. Fade in effect adjusts depending on screen width. Fixed nav bar link heights from overlapping outside of nav bar.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const images = document.querySelectorAll('section img'); 
 
+// Image fade in when top of image meets bottom of screen
+// If window is over 1000px wide, slide in AND fade in (on small desktop size, and larger)
+// If window is under 1000px, only fade in (on tablet size, and smaller)
 function slide(){
     const screenBottom = window.innerHeight + document.body.scrollTop;
     
     images.forEach(image => {
-        screenBottom >= image.offsetTop && image.classList.add("fade-in");
+        (screenBottom >= image.offsetTop && window.innerWidth >= 1000) && image.classList.add('fade-in');
+        (screenBottom >= image.offsetTop && window.innerWidth < 1000) && image.classList.add('fade-in-mobile');
     });
 }
 
